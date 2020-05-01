e5e2bd759d9a4104e704df40dd017e319d034281
Lúcás Meier <cronokirby@gmail.com>
2020-05-01 20:56:19 +0200

    Add parsing for arithmetic operators
    This adds parsing for operators with precedence and associativity. PEG
    makes this very easy to do.
    
    One problem is that we don't handle whitespace between operators. To fix
    this, the best recourse is probably adding a lexer phase, and then
    passing tokens rather than a string to the parser generator.

--- ovis/src/parser.rs
+++ ovis/src/parser.rs
@@ -1,15 +1,42 @@
 use peg;
 use std::error::Error;
 
+/// Represents a binary operator we can use in arithmetic expressions
+#[derive(Debug, PartialEq)]
+pub enum BinOp {
+    /// The + operator
+    Add,
+    /// The - operator
+    Sub,
+    /// The * operator
+    Mul,
+    /// The / operator
+    Div,
+}
+
 #[derive(Debug, PartialEq)]
 pub enum AST {
     NumberLitt(i64),
+    Binary(BinOp, Box<AST>, Box<AST>),
 }
 
 peg::parser! {
     grammar ast_parser() for str {
-        pub rule ast() -> AST
+        rule number_litt() -> AST
             = n:$("-"? ['0'..='9']+) { AST::NumberLitt(n.parse().unwrap()) }
+
+        rule arithmetic() -> AST = precedence!{
+            a:(@) "+" b:@ { AST::Binary(BinOp::Add, Box::new(a), Box::new(b)) }
+            a:(@) "-" b:@ { AST::Binary(BinOp::Sub, Box::new(a), Box::new(b)) }
+            --
+            a:(@) "*" b:@ { AST::Binary(BinOp::Mul, Box::new(a), Box::new(b)) }
+            a:(@) "/" b:@ { AST::Binary(BinOp::Div, Box::new(a), Box::new(b)) }
+            --
+            n:number_litt() { n }
+            "(" e:arithmetic() ")" { e }
+        }
+
+        pub rule ast() -> AST = e:arithmetic() { e }
     }
 }
 
@@ -38,6 +65,9 @@ mod test {
     macro_rules! assert_parse {
         ($a:expr, $b:expr) => {{
             let res = parse($a);
+            if !res.is_ok() {
+                dbg!(&res);
+            }
             assert!(res.is_ok());
             assert_eq!(res.unwrap(), $b);
         }};
@@ -47,5 +77,37 @@ mod test {
     fn parsing_numbers_works() {
         assert_parse!("-69", AST::NumberLitt(-69));
         assert_parse!("69", AST::NumberLitt(69));
+    }
+
+    macro_rules! binary {
+        ($op:expr, $a:literal, $b:literal) => {
+            binary!($op, AST::NumberLitt($a), AST::NumberLitt($b))
+        };
+        ($op:expr, $a:expr, $b:expr) => {
+            AST::Binary($op, Box::new($a), Box::new($b))
+        };
+    }
+
+    #[test]
+    fn parsing_arithmetic_works() {
+        assert_parse!("2+2", binary!(BinOp::Add, 2, 2));
+        assert_parse!("2*2", binary!(BinOp::Mul, 2, 2));
+        assert_parse!("2/2", binary!(BinOp::Div, 2, 2));
+        assert_parse!(
+            "2/3/(4/5)",
+            binary!(
+                BinOp::Div,
+                binary!(BinOp::Div, 2, 3),
+                binary!(BinOp::Div, 4, 5)
+            )
+        );
+        assert_parse!(
+            "2*3+4*5",
+            binary!(
+                BinOp::Add,
+                binary!(BinOp::Mul, 2, 3),
+                binary!(BinOp::Mul, 4, 5)
+            )
+        );
     }
 }
