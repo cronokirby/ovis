5ffc300b3c14fcd66dc298061d92f0c149de6aa9
Lúcás Meier <cronokirby@gmail.com>
2020-08-15 09:24:33 +0200

    Create an Interner with the basic structure
    This will be useful to actually transform the AST

--- src/interner.rs
+++ src/interner.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::hash::Hash;
+
 /// A name we can use for an identifier.
 ///
 /// This idea is that anywhere we could have used a string based identifier,
@@ -24,13 +26,62 @@ pub struct Dictionary {
 }
 
 impl Dictionary {
-    fn new(map: HashMap<Ident, String>) -> Self {
-        Dictionary { map }
+    fn new() -> Self {
+        Dictionary {
+            map: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, ident: Ident, name: String) {
+        self.map.insert(ident, name);
     }
 
     /// Retreive the string connected to a given identifier, if it exists
     pub fn get(&self, ident: Ident) -> Option<&str> {
         // I wonder if we can avoid the map here
         self.map.get(&ident).map(|t| t.as_ref())
+    }
+}
+
+/// The identifier for the I64 known string
+pub const IDENT_I64: Ident = Ident(0);
+
+/// Represents a bidirectional mapping
+struct Interner {
+    dict: Dictionary,
+    lookup: HashMap<String, Ident>,
+    next: Ident,
+}
+
+impl Interner {
+    // Create a new interner, which will contain the built-in identifiers we
+    // know of as well
+    fn new() -> Self {
+        let mut interner = Interner {
+            dict: Dictionary::new(),
+            lookup: HashMap::new(),
+            next: Ident(0),
+        };
+        interner.insert("I64".to_string());
+        interner
+    }
+
+    // Insert a new string, giving it a new identifier, and incrementing the state
+    // of the identifier, and what not
+    fn insert(&mut self, v: String) -> Ident {
+        let key = self.next;
+        self.next = self.next.succ();
+        self.dict.insert(key, v.clone());
+        self.lookup.insert(v, key);
+        key
+    }
+
+    // Get the identifier that a string should have, either looking
+    // up what it is, or creating a new identifier for it if it doesn't have one
+    fn get_or_insert(&mut self, v: String) -> Ident {
+        match self.lookup.get(&v) {
+            Some(x) => *x,
+            None => self.insert(v),
+        }
     }
 }
