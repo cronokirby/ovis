cb35cd29578a64dc0296e90661b9fb57bfeaab59
Lúcás Meier <cronokirby@gmail.com>
2020-08-18 18:01:53 +0200

    Create an error for handling not being able to read a file

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use std::error::Error;
 use std::fmt;
 use std::fs;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 enum CompileError {
+    /// We couldn't read a file while trying to compile it
+    CouldntRead(String),
     /// An error occurring while lexing
     LexError(Vec<lexer::LexError>),
     /// An error occurring while parsing
@@ -21,6 +23,7 @@ enum CompileError {
 impl fmt::Display for CompileError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            CompileError::CouldntRead(path) => writeln!(f, "Failed to read file: {}", path),
             CompileError::LexError(errors) => {
                 writeln!(f, "Errors encountered during Lexing:")?;
                 for e in errors {
@@ -28,8 +31,8 @@ impl fmt::Display for CompileError {
                 }
                 Ok(())
             }
-            CompileError::ParseError(e) => write!(f, "Parse Error: {}", e),
-            CompileError::TypeError(e) => write!(f, "Type Error: {}", e),
+            CompileError::ParseError(e) => writeln!(f, "Parse Error: {}", e),
+            CompileError::TypeError(e) => writeln!(f, "Type Error: {}", e),
         }
     }
 }
@@ -37,6 +40,7 @@ impl fmt::Display for CompileError {
 impl Error for CompileError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
+            CompileError::CouldntRead(_) => None,
             CompileError::LexError(_) => None,
             CompileError::ParseError(e) => Some(e),
             CompileError::TypeError(e) => Some(e),
@@ -62,8 +66,10 @@ impl From<typer::TypeError> for CompileError {
     }
 }
 
-fn parse_and_type(contents: &str) -> Result<ast::AST<interner::Ident, typer::Type>, CompileError> {
-    let tokens = lexer::lex(contents)?;
+fn parse_and_type(path: &str) -> Result<ast::AST<interner::Ident, typer::Type>, CompileError> {
+    let contents =
+        fs::read_to_string(path).map_err(|_| CompileError::CouldntRead(path.to_owned()))?;
+    let tokens = lexer::lex(&contents)?;
     let ast = parser::parse(&tokens)?;
     let interned = interner::intern(ast);
     let typed = typer::typer(interned.ast)?;
@@ -75,13 +81,10 @@ fn main() {
     match all_args.get(1).map(String::as_str) {
         Some("compile") => match all_args.get(2) {
             None => println!("Insufficient arguments"),
-            Some(path) => {
-                let contents = fs::read_to_string(path).expect("Couldn't read file");
-                match parse_and_type(&contents) {
-                    Err(e) => println!("{}", e),
-                    Ok(ast) => println!("Typed AST: {:?}", ast),
-                }
-            }
+            Some(path) => match parse_and_type(path) {
+                Err(e) => println!("{}", e),
+                Ok(ast) => println!("Typed AST: {:?}", ast),
+            },
         },
         _ => println!("Insufficient arguments"),
     }
