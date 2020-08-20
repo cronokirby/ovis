70b8ec9a0603c699c64ab6bca1364cbd48f3f544
Lúcás Meier <cronokirby@gmail.com>
2020-08-20 18:03:45 +0200

    Create a more complex structure of types in the typer
    This introduces a full tree allowing us represent uncertainty precisely
    while type-checking. So we can have the type (? -> I64) -> String, which
    represents a type where just a single part of the function is not yet
    known. This is necessary to be able to elaborate different parts as we
    come onto them.
    
    To do this while minimizing repetition, we divide the type structure
    into the base types, and then the known combinators (just ->). We end up
    with just the concrete types as base types eventually, but while type
    checking we also include ? as a type.

--- src/typer.rs
+++ src/typer.rs
@@ -4,18 +4,45 @@ use crate::interner::Ident;
 use std::error::Error;
 use std::fmt;
 
-/// Represents a fully evaluated Type for some expression or identifier
+/// Represents a base type in our language
 ///
-/// We'll use this as the type to fill in our typed AST later
-#[derive(Clone, Debug, PartialEq)]
-pub enum Type {
+/// The only basic types in our language are the primitive integer
+/// and string types.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum BaseType {
     /// The primitive integer type
     I64,
     /// The primitive string type
     String,
-    /// A function type between two other types
-    Function(Box<Type>, Box<Type>),
 }
+
+/// Represents the recursive structure of types.
+///
+/// This is parametrized over the kind of basic types we have. This is useful
+/// to have the same structure over potentially unknown types. This allows us
+/// to have some type as (? -> A) (for example), in order to have just part of
+/// the full type be unknown.
+#[derive(Clone, Debug, PartialEq)]
+pub enum TypeStructure<T> {
+    Function(Box<T>, Box<T>),
+}
+
+/// Represents a fully evaluated type for some expression
+pub type Type = TypeStructure<BaseType>;
+
+/// A base type where we can represent not knowing what type something is.
+///
+/// This is useful to us as an intermediate stage, allowing us to deal with unknowns
+/// in arbitrary locations, until we know nothing more can be learned about some
+/// expression, forcing us to provide an unambiguous type.
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum MaybeBase {
+    Unknown,
+    Known(BaseType),
+}
+
+/// A full set of types, where the basic types can potentially include the absence of knowledge.
+type MaybeType = TypeStructure<MaybeBase>;
 
 /// This represents the errors that can occurr will assigning types to the program tree
 #[derive(Copy, Clone, Debug, PartialEq)]
