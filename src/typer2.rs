c04e6d8598a9a7abfe1be3178984dd9d22c14dc9
Lúcás Meier <cronokirby@gmail.com>
2020-09-06 15:14:09 +0200

    Create a `Substitutable` abstraction
    This allows us to replace different type variables with concretions,
    which is useful as a final result of type-checking. The goal will be to
    replace a bunch of type variables with final concrete changes.

--- src/typer2.rs
+++ src/typer2.rs
@@ -1,12 +1,109 @@
 use crate::simplifier::{Ident, IdentSource, Scheme, Type};
 use std::collections::{HashMap, HashSet};
 
+/// A synonym for identifiers belonging to type variables
+type TypeVar = Ident;
+
+/// A synonym for identifiers belonging to variables
+type Var = Ident;
+
+/// A substitution maps type variables to fully instantiated types.
+///
+/// The idea is that our end goal is to be able to replace all of the type variables
+/// we've gathered using a concrete substitution scheme, in a way that satisfies
+/// all the constraints on what types are possible through observing how different variables
+/// are used throughout the program.
+struct Substitution(HashMap<TypeVar, Type>);
+
+impl Substitution {
+    /// A substition that replaces no type variables
+    fn empty() -> Self {
+        Substitution(HashMap::new())
+    }
+
+    /// Add a new binding to this substitution
+    fn with(&mut self, t_var: TypeVar, typ: Type) {
+        self.0.insert(t_var, typ);
+    }
+
+    /// Try and find the type corresponding to a given variable, if any
+    fn get(&self, t_var: TypeVar) -> Option<&Type> {
+        self.0.get(&t_var)
+    }
+
+    /// Clone this substitution to another, without including a set of type variables
+    ///
+    /// This is useful when editing a substitution to not include certain bound variables.
+    fn clone_without(&self, vars: &HashSet<TypeVar>) -> Self {
+        let mut ret = Substitution::empty();
+        for (k, v) in &self.0 {
+            if !vars.contains(k) {
+                ret.with(*k, v.clone());
+            }
+        }
+        ret
+    }
+}
+
+/// Represents some kind of type in which we can apply substitutions
+trait Substitutable {
+    /// Apply a given substitution to this type
+    fn subst(&mut self, map: &Substitution);
+
+    /// Calculate the free type variables present in this type
+    fn free_t_vars(&self, buf: &mut HashSet<TypeVar>);
+}
+
+impl Substitutable for Type {
+    fn subst(&mut self, map: &Substitution) {
+        match self {
+            Type::Function(l, r) => {
+                l.subst(map);
+                r.subst(map);
+            }
+            Type::TypeVar(n) => {
+                if let Some(t) = map.get(*n) {
+                    *self = t.clone();
+                }
+            }
+            Type::I64 | Type::Strng => {}
+        }
+    }
+
+    fn free_t_vars(&self, buf: &mut HashSet<TypeVar>) {
+        match self {
+            Type::Function(l, r) => {
+                l.free_t_vars(buf);
+                r.free_t_vars(buf);
+            }
+            Type::TypeVar(n) => {
+                buf.insert(*n);
+            }
+            Type::I64 | Type::Strng => {}
+        }
+    }
+}
+
+impl Substitutable for Scheme {
+    fn subst(&mut self, map: &Substitution) {
+        let no_bound = map.clone_without(&self.type_vars);
+        self.typ.subst(&no_bound);
+    }
+
+    fn free_t_vars(&self, buf: &mut HashSet<TypeVar>) {
+        self.typ.free_t_vars(buf);
+        for v in &self.type_vars {
+            buf.remove(v);
+        }
+    }
+}
+
 /// Represents an environment mapping names to schemes.
 ///
 /// This environment supports scoping, where we can enter and exit scopes,
 /// handling things like shadowing and having temporary bindings.
 struct ScopedEnv {
-    scopes: Vec<HashMap<Ident, Scheme>>,
+    scopes: Vec<HashMap<Var, Scheme>>,
 }
 
 impl ScopedEnv {
@@ -20,14 +117,14 @@ impl ScopedEnv {
     }
 
     /// Extend the environment by mapping a name into a specific scheme
-    fn extend(&mut self, name: Ident, scheme: Scheme) {
+    fn extend(&mut self, name: Var, scheme: Scheme) {
         // We throw an error if scopes becomes empty upon exiting a scope
         let last = self.scopes.last_mut().unwrap();
         last.insert(name, scheme);
     }
 
     /// Find the scheme associated with a certain identifier
-    fn lookup(&self, name: Ident) -> Option<&Scheme> {
+    fn lookup(&self, name: Var) -> Option<&Scheme> {
         for scope in self.scopes.iter().rev() {
             if let Some(scheme) = scope.get(&name) {
                 return Some(scheme);
@@ -51,6 +148,24 @@ impl ScopedEnv {
             panic!("UNTHINKABLE: Tried to exit global scope!")
         }
         self.scopes.pop();
+    }
+}
+
+impl Substitutable for ScopedEnv {
+    fn subst(&mut self, map: &Substitution) {
+        for scope in &mut self.scopes {
+            for typ in scope.values_mut() {
+                typ.subst(map);
+            }
+        }
+    }
+
+    fn free_t_vars(&self, buf: &mut HashSet<TypeVar>) {
+        for scope in &self.scopes {
+            for typ in scope.values() {
+                typ.free_t_vars(buf);
+            }
+        }
     }
 }
 
