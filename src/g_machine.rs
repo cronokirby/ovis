98bcbdbed8d8004b44ac2036548d763ec87ccdb9
Lúcás Meier <cronokirby@gmail.com>
2020-10-11 21:23:58 +0200

    Create the basic structure for interpretation

--- src/g_machine.rs
+++ src/g_machine.rs
@@ -1,5 +1,6 @@
 use crate::identifiers::Ident;
 use crate::parser::BinOp;
+use std::collections::HashMap;
 
 /// Represents an instruction for our abstract machine.
 ///
@@ -32,4 +33,70 @@ enum Instruction {
     Eval,
     /// Allocate a placeholder for some node
     Alloc,
+}
+
+#[derive(Clone, Debug)]
+struct Pointer(u64);
+
+#[derive(Debug)]
+struct Frame {
+    instructions: Vec<Instruction>,
+    stack: Vec<Pointer>,
+}
+
+#[derive(Clone, Debug)]
+/// Represents the type of items on the heap.
+enum HeapItem {
+    /// An integer on the heap
+    I64(i64),
+    /// A string contained on the heap
+    Strng(String),
+    /// An indirection pointing to some other item on the heap
+    Ind(Pointer),
+}
+
+/// Represents a heap where we can allocate scratch memory for nodes
+///
+/// This is completely un-garbage collected. Once we've added a node to the Heap,
+/// we're not going to ever remove it.
+#[derive(Debug)]
+struct Heap(Vec<HeapItem>);
+
+impl Heap {
+    /// Create a new Heap.
+    fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    /// Allocate a new item onto the Heap, returning its address
+    fn alloc(&mut self, item: HeapItem) -> Pointer {
+        self.0.push(item);
+        Pointer(self.0.len() as u64 - 1)
+    }
+
+    /// Retrieve a reference to the item at a certain address in the heap
+    fn at(&self, p: Pointer) -> &HeapItem {
+        &self.0[p.0 as usize]
+    }
+}
+
+#[derive(Debug)]
+struct Machine {
+    instructions: Vec<Instruction>,
+    stack: Vec<Pointer>,
+    dump: Vec<Frame>,
+    heap: Heap,
+    globals: HashMap<Ident, Pointer>,
+}
+
+impl Machine {
+    fn new(instructions: Vec<Instruction>) -> Self {
+        Self {
+            instructions,
+            stack: Vec::new(),
+            dump: Vec::new(),
+            heap: Heap::new(),
+            globals: HashMap::new(),
+        }
+    }
 }
